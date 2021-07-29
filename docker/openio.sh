5f5c38aacbb3e43b31e52c36efa68004f93a7c78
doixanh <doixanh@gmail.com>
2021-07-29 17:20:48 +0700

    docker: Update launch openio command

--- docker/openio.sh
+++ docker/openio.sh
@@ -2,11 +2,6 @@
 
 docker run \
   --name ulake-openio \
-  -v $PWD/openio/metadata-level0:/var/lib/oio/sds/OPENIO/meta0-0 \
-  -v $PWD/openio/metadata-level1:/var/lib/oio/sds/OPENIO/meta1-0 \
-  -v $PWD/openio/metadata-level2:/var/lib/oio/sds/OPENIO/meta2-0 \
-  -v $PWD/openio/accounting:/var/lib/oio/sds/OPENIO/redis-0 \
-  -v $PWD/openio/data:/var/lib/oio/sds/OPENIO/rawx-0 \
-  -e SWIFT_CREDENTIALS="ulake:admin:abc123:.admin" \
+  -e OPENIO_IFDEV=br0 --net=host \
   -d \
-  openio/sds:20.04
+  openio/sds
