d892364f71e23e225860a07edd5b76a8e897115e
doixanh <doixanh@gmail.com>
2021-07-31 18:48:56 +0700

    api: Add skeleton for api project

---
+++ api/src/test/java/org/usth/ict/ulake/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.usth.ict.ulake.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
