eeebce1100855e96fbc76d55bde6380deb1755c9
doixanh <doixanh@gmail.com>
2021-08-01 11:47:50 +0700

    ulake: WIP: migrating from Spring Boot to Quarkus

--- core/src/main/java/org/usth/ict/ulake/core/persistence/DatasetRepository.java
+++ core/src/main/java/org/usth/ict/ulake/core/persistence/DatasetRepository.java
@@ -1,6 +1,5 @@
 package org.usth.ict.ulake.core.persistence;
 
-import org.springframework.data.jpa.repository.JpaRepository;
 import org.usth.ict.ulake.core.model.LakeDataset;
 
 public interface DatasetRepository extends JpaRepository<LakeDataset, Long> {
