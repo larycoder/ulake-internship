92d6ab40d7849ed33718fc7817f7b010dbee2df5
doixanh <doixanh@gmail.com>
2021-08-07 10:48:38 +0700

    core: Add support to create new dataset

--- core/src/main/java/org/usth/ict/ulake/core/resource/DatasetResource.java
+++ core/src/main/java/org/usth/ict/ulake/core/resource/DatasetResource.java
@@ -3,9 +3,11 @@ package org.usth.ict.ulake.core.resource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.usth.ict.ulake.core.model.LakeDataset;
+import org.usth.ict.ulake.core.model.LakeHttpResponse;
 import org.usth.ict.ulake.core.persistence.DatasetRepository;
 
 import javax.inject.Inject;
+import javax.transaction.Transactional;
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
@@ -19,14 +21,19 @@ public class DatasetResource {
     @Inject
     DatasetRepository datasetRepo;
 
+    @Inject
+    LakeHttpResponse lakeHttpResponse;
+
     @GET
     public List<LakeDataset> all() {
         return datasetRepo.listAll();
     }
 
     @POST
+    @Transactional
     @Consumes(MediaType.APPLICATION_JSON)
     public Response post(LakeDataset dataset) {
-        return Response.ok().build();
+        datasetRepo.persist(dataset);
+        return lakeHttpResponse.build(200, "", dataset);
     }
 }
