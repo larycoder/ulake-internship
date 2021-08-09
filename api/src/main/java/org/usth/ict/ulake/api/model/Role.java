527e0d69bf7247ff42e87299d3080edebf6508ec
dx <sontg.usth@gmail.com>
2021-08-09 23:59:40 +0700

    api: add some desc for Role class

--- api/src/main/java/org/usth/ict/ulake/api/model/Role.java
+++ api/src/main/java/org/usth/ict/ulake/api/model/Role.java
@@ -6,6 +6,9 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
+/**
+ * A typical role, like read, write, list
+ */
 public class Role  extends PanacheEntityBase {
     @Id
     @GeneratedValue(strategy= GenerationType.AUTO)
