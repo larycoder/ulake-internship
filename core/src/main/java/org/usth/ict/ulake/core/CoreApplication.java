1ecbc03de002df1f410bafbde4ef5a58702098ac
doixanh <doixanh@gmail.com>
2021-08-17 16:48:48 +0700

    ulake: Add /api prefix for all projects

---
+++ core/src/main/java/org/usth/ict/ulake/core/CoreApplication.java
@@ -0,0 +1,8 @@
+package org.usth.ict.ulake.core;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class CoreApplication extends Application {
+}
---
+++ folder/src/main/java/org/usth/ict/ulake/folder/FolderApplication.java
@@ -0,0 +1,8 @@
+package org.usth.ict.ulake.folder;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class FolderApplication extends Application {
+}
