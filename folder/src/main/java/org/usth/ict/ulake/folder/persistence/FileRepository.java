b00bce65955af764a5abe9fa72f34a0b4a258b44
doixanh <doixanh@gmail.com>
2021-08-17 16:36:05 +0700

    folder: Add missing bean annotation for repository

--- folder/src/main/java/org/usth/ict/ulake/folder/persistence/FileRepository.java
+++ folder/src/main/java/org/usth/ict/ulake/folder/persistence/FileRepository.java
@@ -3,5 +3,8 @@ package org.usth.ict.ulake.folder.persistence;
 import io.quarkus.hibernate.orm.panache.PanacheRepository;
 import org.usth.ict.ulake.folder.model.UserFile;
 
+import javax.enterprise.context.ApplicationScoped;
+
+@ApplicationScoped
 public class FileRepository implements PanacheRepository<UserFile> {
 }
--- folder/src/main/java/org/usth/ict/ulake/folder/persistence/FolderRepository.java
+++ folder/src/main/java/org/usth/ict/ulake/folder/persistence/FolderRepository.java
@@ -3,5 +3,8 @@ package org.usth.ict.ulake.folder.persistence;
 import io.quarkus.hibernate.orm.panache.PanacheRepository;
 import org.usth.ict.ulake.folder.model.UserFolder;
 
+import javax.enterprise.context.ApplicationScoped;
+
+@ApplicationScoped
 public class FolderRepository implements PanacheRepository<UserFolder> {
 }
