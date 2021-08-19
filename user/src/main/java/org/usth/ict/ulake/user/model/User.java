8bd855c62bc89b2a01b511033768350839faf507
dx <doixanh@gmail.com>
2021-08-19 09:37:06 +0700

    user: Add length for access token

--- user/src/main/java/org/usth/ict/ulake/user/model/User.java
+++ user/src/main/java/org/usth/ict/ulake/user/model/User.java
@@ -19,6 +19,8 @@ public class User extends PanacheEntityBase {
     public String email;
     public String password;
     public Long registerTime;
+
+    @Column(columnDefinition = "varchar(5000)")
     public String accessToken;
     public String refreshToken;
     public Long refreshTokenExpire;
