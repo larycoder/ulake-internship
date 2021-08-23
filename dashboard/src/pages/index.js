bc4cb249610bd53c166eb2c3f2e0eed9740cc594
dx <doixanh@gmail.com>
2021-08-23 23:28:29 +0700

    dashboard: Use Hooks instead of classes

--- dashboard/src/pages/index.js
+++ dashboard/src/pages/index.js
@@ -1,4 +1,4 @@
-import * as React from 'react'
+import React, { useEffect, useState } from 'react'
 import axios from 'axios'
 import Layout from "../components/layout";
 import { Link } from 'gatsby';
@@ -8,47 +8,47 @@ import { useTable } from 'react-table';
 function Table({ columns, data }) {
     // Use the state and functions returned from useTable to build your UI
     const {
-      getTableProps,
-      getTableBodyProps,
-      headerGroups,
-      rows,
-      prepareRow,
+        getTableProps,
+        getTableBodyProps,
+        headerGroups,
+        rows,
+        prepareRow,
     } = useTable({
-      columns,
-      data,
+        columns,
+        data,
     })
-  
+
     // Render the UI for your table
     return (
-      <table {...getTableProps()}>
-        <thead>
-          {headerGroups.map(headerGroup => (
-            <tr {...headerGroup.getHeaderGroupProps()}>
-              {headerGroup.headers.map(column => (
-                <th {...column.getHeaderProps()}>{column.render('Header')}</th>
-              ))}
-            </tr>
-          ))}
-        </thead>
-        <tbody {...getTableBodyProps()}>
-          {rows.map((row, i) => {
-            prepareRow(row)
-            return (
-              <tr {...row.getRowProps()}>
-                {row.cells.map(cell => {
-                  return <td {...cell.getCellProps()}>{cell.render('Cell')}</td>
+        <table {...getTableProps()}>
+            <thead>
+                {headerGroups.map(headerGroup => (
+                    <tr {...headerGroup.getHeaderGroupProps()}>
+                        {headerGroup.headers.map(column => (
+                            <th {...column.getHeaderProps()}>{column.render('Header')}</th>
+                        ))}
+                    </tr>
+                ))}
+            </thead>
+            <tbody {...getTableBodyProps()}>
+                {rows.map((row, i) => {
+                    prepareRow(row)
+                    return (
+                        <tr {...row.getRowProps()}>
+                            {row.cells.map(cell => {
+                            return <td {...cell.getCellProps()}>{cell.render('Cell')}</td>
+                            })}
+                        </tr>
+                    )
                 })}
-              </tr>
-            )
-          })}
-        </tbody>
-      </table>
+            </tbody>
+        </table>
     )
-  }
+}
 
-class IndexQuery extends React.Component {
-    columns = [
-        {
+function Index() {
+    const [users, setUsers] = useState([]);
+    const columns = [{
             Header: 'Name',
             accessor: 'name',
         },
@@ -56,40 +56,29 @@ class IndexQuery extends React.Component {
             Header: 'Email',
             accessor: 'email',
         },
-    ]    
-    state = {
-        users: []
-    }
-    loadUsers = async () => {;
-        const users = await axios.get("http://user.ulake.sontg.net/api/user");
-        if (users.data.code === 200) {
-            let extractUsers = users.data.resp.map(user => {return {name: user.userName, email: user.email}});
-            this.setState({users: extractUsers});
+    ];
+    
+    (async function fetchUsers(url) {
+        const ajaxUsers = await axios.get(url);
+        if (ajaxUsers.data.code === 200) {            
+            const nameEmails = ajaxUsers.data.resp.map(user => {return {name: user.userName, email: user.email}});
+            setUsers(nameEmails);
         }
-    }
+    })("http://user.ulake.sontg.net/api/user");
 
-    componentDidMount() {
-        this.loadUsers();
-    }
-
-    render() {
-        const {users} = this.state;
-        return (
-            <Layout pageTitle="Home Page">
-                <p>List of available users</p>
-                <ul>
+    return (
+        <Layout pageTitle="Home Page">
+            <p>List of available users</p>
+            <ul>
                 {users.map(user => (
-                    <li key={user.name}>
-                        <Link to={`./user/${user.name}`}>
-                            {user.name}, {user.email}
-                        </Link>
-                    </li>
+                <li key={user.name}>
+                    <Link to={`./user/${user.name}`}>{user.name}, {user.email}</Link>
+                </li>
                 ))}
-                </ul>
-                <Table columns={this.columns} data={users} />
-            </Layout>
-        )
-    }
+            </ul>
+            <Table columns={columns} data={users} />
+        </Layout>
+    )
 }
 
-export default IndexQuery
+export default Index
