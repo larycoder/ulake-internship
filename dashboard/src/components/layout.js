e349dbd0d638f17d51bfe0c8758cb17522d6a489
dx <doixanh@gmail.com>
2021-08-24 22:39:39 +0700

    dashboard: Split user list to a new page

--- dashboard/src/components/layout.js
+++ dashboard/src/components/layout.js
@@ -15,6 +15,7 @@ const Layout = ({ pageTitle, children }) => {
             <nav>
                 <ul className={navLinks}>
                     <li className={navLinkItem}><Link to="/" className={navLinkText}>Home</Link></li>
+                    <li className={navLinkItem}><Link to="/users" className={navLinkText}>Users</Link></li>
                     <li className={navLinkItem}><Link to="/about" className={navLinkText}>About</Link></li>
                 </ul>
             </nav>
