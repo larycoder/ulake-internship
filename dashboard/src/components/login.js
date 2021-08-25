b1a1a31014a3bb9d7c6d29d961227be55ab600f3
doixanh <doixanh@gmail.com>
2021-08-25 15:10:12 +0700

    dashboard: Use React hooks instead of class

--- dashboard/src/components/login.js
+++ dashboard/src/components/login.js
@@ -1,52 +1,58 @@
-import React from "react"
+import React, { useState } from "react"
 import { navigate } from "gatsby"
 import { handleLogin, isLoggedIn } from "../services/auth"
 import Layout from "./layout"
 
-class Login extends React.Component {
-    state = {
+
+function handleUpdate(event, setState) {
+    setState({
+        [event.target.name]: event.target.value,
+    });
+}
+
+function handleSubmit(event, state) {
+    event.preventDefault();
+    handleLogin(state);
+}
+
+
+function Login() {
+    if (isLoggedIn()) {
+        navigate(`/dashboard/profile`)
+    }
+
+    const [state, setState] = useState({
         username: ``,
         password: ``,
-    }
-    handleUpdate = event => {
-        this.setState({
-            [event.target.name]: event.target.value,
-        })
-    }
-    handleSubmit = event => {
-        event.preventDefault()
-        handleLogin(this.state)
-    }
-    render() {
-        if (isLoggedIn()) {
-            navigate(`/dashboard/profile`)
-        }
-        return (
-            <Layout>
-                <h1>Log in</h1>
-                    <form
-                        method="post"
-                        onSubmit={event => {
-                            this.handleSubmit(event)
-                            navigate(`/dashboard/profile`)
-                        }}
-                    >
-                    <label>
-                        Username
-                        <input type="text" name="username" onChange={this.handleUpdate} />
-                    </label>
-                    <label>
-                        Password
-                        <input
-                            type="password"
-                            name="password"
-                            onChange={this.handleUpdate}
-                        />
-                    </label>
-                    <input type="submit" value="Log In" />
-                </form>
-            </Layout>
-        )
-    }
+    });
+
+
+    return (
+        <Layout>
+            <h1>Log in</h1>
+            <form
+                method="post"
+                onSubmit={event => {
+                    handleSubmit(event, state);
+                    navigate(`/dashboard/profile`);
+                }}
+            >
+                <label>
+                    Username
+                    <input type="text" name="username" onChange={handleUpdate} />
+                </label>
+                <label>
+                    Password
+                    <input
+                        type="password"
+                        name="password"
+                        onChange={handleUpdate}
+                    />
+                </label>
+                <input type="submit" value="Log In" />
+            </form>
+        </Layout>
+    )
 }
+
 export default Login
