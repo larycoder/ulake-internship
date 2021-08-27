eed385a5aae787dfd8e09d3817df6b53a8421118
doixanh <doixanh@gmail.com>
2021-08-27 11:56:06 +0700

    dashboard: Use consts instead of hardcoded action values

--- dashboard/src/pages/index.js
+++ dashboard/src/pages/index.js
@@ -4,34 +4,35 @@ import { isLoggedIn, getUser } from "../services/auth"
 import { connect } from 'react-redux'
 import PropTypes from "prop-types"
 import { obtainJwt } from '../state/jwt';
+import { actions } from '../state/createStore';
 
 const Counter = ({ count, increment }) => (
     <div>
-      <p>Count: {count}</p>
-      <button onClick={increment}>Increment</button>
+        <p>Count: {count}</p>
+        <button onClick={increment}>Increment</button>
     </div>
-  )
-  
-  Counter.propTypes = {
+)
+
+Counter.propTypes = {
     count: PropTypes.number.isRequired,
     increment: PropTypes.func.isRequired,
-  }
-  
-  const mapStateToProps = ({ count }) => {
+}
+
+const mapStateToProps = ({ count }) => {
     return { count }
-  }
-  
-  const mapDispatchToProps = dispatch => {
-    return { increment: () => dispatch({ type: `INCREMENT` }) }
-  }
-  
-  const ConnectedCounter = connect(mapStateToProps, mapDispatchToProps)(Counter)
+}
+
+const mapDispatchToProps = dispatch => {
+    return { increment: () => dispatch({ type: actions.increment }) }
+}
+
+const ConnectedCounter = connect(mapStateToProps, mapDispatchToProps)(Counter)
 
 const Index = () => (
     <Layout pageTitle="Home Page">
-        <p>Welcome back.</p>       
-        <ConnectedCounter />     
-        <h1>Hello {isLoggedIn() ? getUser().name : "world"}!</h1>        
+        <p>Welcome back.</p>
+        <ConnectedCounter />
+        <h1>Hello {isLoggedIn() ? getUser().name : "world"}!</h1>
     </Layout>
 )
 
--- dashboard/src/state/createStore.js
+++ dashboard/src/state/createStore.js
@@ -1,16 +1,22 @@
-  
+
 import { createStore as reduxCreateStore } from "redux"
 
+const actions = {
+    increment: `INCREMENT`
+}
+
 const reducer = (state, action) => {
-  if (action.type === `INCREMENT`) {
-    return Object.assign({}, state, {
-      count: state.count + 1,
-    })
-  }
-  return state
+    if (action.type === actions.increment) {
+        return Object.assign({}, state, {
+            count: state.count + 1,
+        })
+    }
+    return state
 }
 
 const initialState = { count: 0 }
 
 const createStore = () => reduxCreateStore(reducer, initialState)
 export default createStore
+export { actions }
+
