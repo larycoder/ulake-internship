fab5dcab40c904805480d0b229613fc5e3a17031
doixanh <doixanh@gmail.com>
2021-09-09 11:37:47 +0700

    user: Fix missing email/password when creating a new user

--- user/src/main/java/org/usth/ict/ulake/user/model/User.java
+++ user/src/main/java/org/usth/ict/ulake/user/model/User.java
@@ -1,13 +1,21 @@
 package org.usth.ict.ulake.user.model;
 
-import com.fasterxml.jackson.annotation.JsonBackReference;
-import io.quarkus.hibernate.orm.panache.PanacheEntityBase;
-
-import javax.persistence.*;
 import java.util.HashSet;
-import java.util.List;
 import java.util.Set;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
+
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import io.quarkus.hibernate.orm.panache.PanacheEntityBase;
 
 @Entity
 public class User extends PanacheEntityBase {
@@ -17,9 +25,11 @@ public class User extends PanacheEntityBase {
     public String userName;
     public String firstName;
     public String lastName;
-    @JsonIgnore
+    
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
     public String email;
-    @JsonIgnore
+    
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
     public String password;
     public Long registerTime;
 
@@ -40,4 +50,23 @@ public class User extends PanacheEntityBase {
     @ManyToMany
     @JoinColumn
     public Set<UserGroup> groups = new HashSet<>();
+
+    @JsonIgnore
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @JsonIgnore
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    
 }
