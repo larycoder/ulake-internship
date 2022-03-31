5fd3b9241a5853958973c408a3c3f31392e7915b
larycoder <hieplnc.m20ict@st.usth.edu.vn>
2022-03-31 10:20:45 +0700

    deploy: link docker mysql data storage to host permanent disk

--- deployment/start-mysql.sh
+++ deployment/start-mysql.sh
@@ -1,15 +1,20 @@
 #!/bin/bash
 
 # Predefine variable
+BASE_DIR=$(readlink -f $(dirname $0));
+
 HOST="ulake-mysql";
 PORT="23306";
 PASSWORD="root"; # for root user
-SQL="./init.sql";
+
+SQL="$BASE_DIR/init.sql";
+DATA="$BASE_DIR/data/mysql";
 TIME=10; # temporary wait for waiting server up
 
 # Start newest mariadb and initialize database
 docker run --detach --name $HOST \
     --env MYSQL_ROOT_PASSWORD="$PASSWORD" \
+    -v $DATA:/var/lib/mysql \
     -p $PORT:3306 mariadb:latest && \
     sleep $TIME && \
     docker exec -i $HOST mysql -u root < $SQL;
