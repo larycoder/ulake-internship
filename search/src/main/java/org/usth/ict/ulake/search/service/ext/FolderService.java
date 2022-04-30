a9d0868101a9ca36670e5016298060670e6c51a7
larycoder <hieplnc.m20ict@st.usth.edu.vn>
2022-04-30 15:55:07 +0700

    search: initialize service sub-package

---
+++ search/src/main/java/org/usth/ict/ulake/search/service/ext/FolderService.java
@@ -0,0 +1,15 @@
+package org.usth.ict.ulake.search.service.ext;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import org.eclipse.microprofile.rest.client.inject.RegisterRestClient;
+
+@Path("/api")
+@RegisterRestClient(configKey="folder-api")
+@Consumes(MediaType.APPLICATION_JSON)
+@Produces(MediaType.APPLICATION_JSON)
+public class FolderService {
+}
