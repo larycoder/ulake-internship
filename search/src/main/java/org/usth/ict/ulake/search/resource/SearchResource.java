0170e547c5ef0efc81064138a119069966dc4393
larycoder <lenhuchuhiep99@gmail.com>
2022-05-04 01:17:35 +0700

    search: [wip] build search shawdow for all service

--- search/src/main/java/org/usth/ict/ulake/search/resource/SearchResource.java
+++ search/src/main/java/org/usth/ict/ulake/search/resource/SearchResource.java
@@ -1,10 +1,11 @@
 package org.usth.ict.ulake.search.resource;
 
-import java.util.List;
 import java.util.ArrayList;
+import java.util.List;
 
 import javax.annotation.security.RolesAllowed;
 import javax.inject.Inject;
+import javax.ws.rs.Consumes;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
@@ -14,17 +15,20 @@ import javax.ws.rs.core.Response;
 import org.eclipse.microprofile.jwt.JsonWebToken;
 import org.eclipse.microprofile.openapi.annotations.Operation;
 import org.eclipse.microprofile.rest.client.inject.RestClient;
-import org.jboss.resteasy.client.exception.ResteasyWebApplicationException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.usth.ict.ulake.common.model.LakeHttpResponse;
+import org.usth.ict.ulake.common.service.exception.LakeServiceException;
+import org.usth.ict.ulake.common.service.exception.LakeServiceNotFoundException;
 import org.usth.ict.ulake.search.model.FilterModel;
 import org.usth.ict.ulake.search.service.SearchService;
+import org.usth.ict.ulake.search.service.ext.FolderService;
 import org.usth.ict.ulake.search.service.ext.UserService;
 import org.usth.ict.ulake.user.model.User;
 
 @Path("/search")
 @Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
 public class SearchResource {
     private static final Logger log = LoggerFactory.getLogger(SearchResource.class);
 
@@ -37,6 +41,10 @@ public class SearchResource {
     @Inject
     @RestClient
     UserService userSvc;
+
+    @Inject
+    @RestClient
+    FolderService folderSvc;
 
     /**
      * Generic function to query data from sub-services
@@ -52,8 +60,11 @@ public class SearchResource {
             String token = "bearer " + jwt.getRawToken();
             var resp = svc.search(token, query);
             return (List<R>) resp.getResp();
-        } catch (ResteasyWebApplicationException e) {
-            log.error("Fail to query {}", svc, e);
+        } catch (LakeServiceNotFoundException e) {
+            log.error("Not found error ({})", svc, e);
+            return new ArrayList<R>();
+        } catch (LakeServiceException e) {
+            log.error("Unknown error ({})", svc, e);
             return null;
         }
     }
@@ -63,6 +74,12 @@ public class SearchResource {
     @Operation(summary = "Search user information")
     public Response user(FilterModel filter) {
         List<User> users = queryFunc(userSvc, filter.userQuery);
-        return response.build(200, null, users);
+        if (users == null) {
+            return response.build(500, "internal error");
+        } else if (users.isEmpty()) {
+            return response.build(404, "not found error");
+        } else {
+            return response.build(200, null, users);
+        }
     }
 }
