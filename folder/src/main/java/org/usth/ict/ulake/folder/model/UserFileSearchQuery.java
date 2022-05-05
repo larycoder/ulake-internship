9b2553b87cd150084c8cb77cd2e66e94c3305c61
larycoder <lenhuchuhiep99@gmail.com>
2022-05-05 12:05:06 +0700

    folder: update user file query to filter by ownerId

--- folder/src/main/java/org/usth/ict/ulake/folder/model/UserFileSearchQuery.java
+++ folder/src/main/java/org/usth/ict/ulake/folder/model/UserFileSearchQuery.java
@@ -1,8 +1,13 @@
 package org.usth.ict.ulake.folder.model;
 
+import java.util.List;
+
 import org.eclipse.microprofile.openapi.annotations.media.Schema;
 
 public class UserFileSearchQuery {
+    @Schema(description = "Filter by file ownerId")
+    public List<Long> ownerIds;
+
     @Schema(description = "A keyword that responded filenames must contain.")
     public String keyword;
 
--- folder/src/main/java/org/usth/ict/ulake/folder/persistence/FileRepository.java
+++ folder/src/main/java/org/usth/ict/ulake/folder/persistence/FileRepository.java
@@ -1,21 +1,34 @@
 package org.usth.ict.ulake.folder.persistence;
 
-import io.quarkus.hibernate.orm.panache.PanacheRepository;
-import org.usth.ict.ulake.common.misc.Utils;
-import org.usth.ict.ulake.folder.model.UserFile;
-import org.usth.ict.ulake.folder.model.UserFileSearchQuery;
-
-import javax.enterprise.context.ApplicationScoped;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+
+import javax.enterprise.context.ApplicationScoped;
+
+import org.usth.ict.ulake.common.misc.Utils;
+import org.usth.ict.ulake.folder.model.UserFile;
+import org.usth.ict.ulake.folder.model.UserFileSearchQuery;
+
+import io.quarkus.hibernate.orm.panache.PanacheRepository;
 
 @ApplicationScoped
 public class FileRepository implements PanacheRepository<UserFile> {
     public List<UserFile> search(UserFileSearchQuery query) {
         ArrayList<String> conditions = new ArrayList<>();
         Map<String, Object> params = new HashMap<>();
+
+        if(query.ownerIds != null && !query.ownerIds.isEmpty()) {
+            var ownerIds = new ArrayList<Long>();
+            for(var ownerId : query.ownerIds) {
+                if(ownerId >= 0)
+                    ownerIds.add(ownerId);
+            }
+            conditions.add("(ownerId in (:ownerIds))");
+            params.put("ownerIds", ownerIds);
+        }
+
         if (!Utils.isEmpty(query.keyword)) {
             conditions.add("(name like :keyword)");
             params.put("keyword", "%" + query.keyword + "%");
