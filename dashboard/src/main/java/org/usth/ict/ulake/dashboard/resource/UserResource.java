f4ae15beae830e619dfe9c6c73e4aca336c43764
larycoder <lenhuchuhiep99@gmail.com>
2022-05-13 09:15:28 +0700

    dashboard: [fix] remove orphan import

--- dashboard/src/main/java/org/usth/ict/ulake/dashboard/resource/UserResource.java
+++ dashboard/src/main/java/org/usth/ict/ulake/dashboard/resource/UserResource.java
@@ -1,7 +1,6 @@
 package org.usth.ict.ulake.dashboard.resource;
 
 import javax.annotation.security.PermitAll;
-import javax.annotation.security.RolesAllowed;
 import javax.inject.Inject;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.POST;
