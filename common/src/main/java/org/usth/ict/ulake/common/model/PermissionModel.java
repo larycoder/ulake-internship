ec2030f75f0610519dabfccaeb7122bca3e09d03
larycoder <lenhuchuhiep99@gmail.com>
2022-05-27 17:44:56 +0700

    common: [fix] change permission to enum

--- common/src/main/java/org/usth/ict/ulake/common/model/PermissionModel.java
+++ common/src/main/java/org/usth/ict/ulake/common/model/PermissionModel.java
@@ -1,23 +1,3 @@
 package org.usth.ict.ulake.common.model;
 
-/**
- * List permission of object.
- * */
-public class PermissionModel {
-    private final static Integer[] permissions = {1, 2, 4};
-
-    public final static Integer EXECUTE = 1;
-    public final static Integer WRITE = 2;
-    public final static Integer READ = 4;
-
-    /**
-     * Validate permission value.
-     * @param value permission value
-     * */
-    public static Boolean isPermission(Integer value) {
-        for (int i = 0; i < permissions.length; i++)
-            if (permissions[i] == value)
-                return true;
-        return false;
-    }
-}
+public enum PermissionModel {READ, WRITE, EXECUTE}
