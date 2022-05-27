6f9b8a319ff0327e5590c4b2a4530aec18ae966e
larycoder <lenhuchuhiep99@gmail.com>
2022-05-27 14:17:53 +0700

    acl: update acl model

---
+++ acl/src/main/java/org/usth/ict/ulake/acl/model/FileAcl.java
@@ -0,0 +1,38 @@
+package org.usth.ict.ulake.acl.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
+
+import org.eclipse.microprofile.openapi.annotations.media.Schema;
+
+@Entity
+@Table(uniqueConstraints = {
+    @UniqueConstraint(
+    columnNames = {"fileId", "userId", "permission"})
+})
+public class FileAcl {
+    @Id
+    @Schema(description = "Marked ID for permission, must not included in query")
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    public Long id;
+
+    @Schema(description = "File ID from file management")
+    public Long fileId;
+
+    @Schema(description = "Flag to mark object is folder")
+    @Column(length = 1)
+    public String isFolder = "0";
+
+    @Schema(description = "User ID from user management")
+    public Long userId;
+
+    @Schema(description = "Permission of object for corresponding user")
+    public Integer permission;
+
+    public FileAcl() {}
+}
