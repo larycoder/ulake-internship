f59ebbfa2a5f5d73b51f5887c22ef7b327ccaeea
larycoder <hieplnc.m20ict@st.usth.edu.vn>
2022-05-30 13:46:19 +0700

    ulake: add common service for html

--- deployment/start-nginx.sh
+++ deployment/start-nginx.sh
@@ -10,10 +10,12 @@ ROOT_DIR=$(readlink -f $BASE_DIR/../);
 CONF="$BASE_DIR/nginx.conf";
 
 DASHBOARD="$ROOT_DIR/dashboard/html";
+COMMON="$ROOT_DIR/common/html";
 ADMIN="$ROOT_DIR/admin/html";
 
 docker run --name $HOST -p $PORT:80 \
     -v $CONF:/etc/nginx/nginx.conf:ro \
     -v $DASHBOARD:/opt/dashboard:ro \
+    -v $COMMON:/opt/common:ro \
     -v $ADMIN:/opt/admin:ro \
     -d nginx:latest
