88e5505c4dfc982e5408124d848d1defc52635b4
dx <doixanh@gmail.com>
2022-06-22 05:04:05 +0000

    table: Parse header

--- table/src/main/java/org/usth/ict/ulake/table/utils/Parser.java
+++ table/src/main/java/org/usth/ict/ulake/table/utils/Parser.java
@@ -4,10 +4,13 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.Reader;
+import java.util.ArrayList;
 
 import org.apache.commons.csv.CSVFormat;
 import org.apache.commons.csv.CSVRecord;
+import org.hibernate.engine.jdbc.ColumnNameCache;
 import org.usth.ict.ulake.table.model.Table;
+import org.usth.ict.ulake.table.model.TableColumnModel;
 import org.usth.ict.ulake.table.model.TableMetadata;
 import org.usth.ict.ulake.table.model.TableModel;
 
@@ -18,17 +21,30 @@ public class Parser {
         table.model.name = metadata.name;
         table.model.format = metadata.format;
 
-        Reader ir = new InputStreamReader(is);        
+        table.columns = new ArrayList<>();
+
+        Reader ir = new InputStreamReader(is);
         try {
             Iterable<CSVRecord> records;
             records = CSVFormat.DEFAULT.parse(ir);
             var it = records.iterator();
-            while (it.hasNext()) {                
+            while (it.hasNext()) {
                 var row = it.next();
-                System.out.println("Row size : " + row.size());                
+                if (table.columns.size() == 0) {
+                    // first row should be header
+                    for (var key: row) {
+                        TableColumnModel column = new TableColumnModel();
+                        column.columnName = key.toString().trim().replaceAll("^\"|\"$", "");
+                        column.dataType = "string";     // default...s
+                        table.columns.add(column);
+                        System.out.println("header " + key.toString().trim());
+                    }
+                }
+
+                System.out.println("Row size : " + row.size());
                 for (var key: row) {
-                    System.out.println("CSV " + key);
-                }                
+                    System.out.println("CSV " + key.toString().trim());
+                }
             }
         } catch (IOException e) {
             e.printStackTrace();
