3f1021565b0217b7b071278fb76002711f681b51
dx <doixanh@gmail.com>
2022-06-22 04:57:32 +0000

    deployment: Add login script

---
+++ deployment/login.sh
@@ -0,0 +1,11 @@
+#!/bin/bash 
+
+token=`curl -s -X POST -H "Content-Type: application/json" -d '{"userName":"dx", "password":"1"}'  http://user.ulake.sontg.net/api/auth/login | grep -o "resp.*" | sed  's/.*":"//;s/"}$//'`
+
+echo This script should be sourced to get JWT exported.
+echo $token
+if [ "`command -v xclip`" != "" ]; then
+	echo $token | xclip
+fi
+
+export JWT=$token
