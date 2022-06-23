849251396dd12a3a34fd298c60cb74cb4c4255e4
larycoder <lenhuchuhiep99@gmail.com>
2022-06-23 15:19:47 +0700

    common: update annotation for acl service

--- common/src/main/java/org/usth/ict/ulake/common/service/AclService.java
+++ common/src/main/java/org/usth/ict/ulake/common/service/AclService.java
@@ -1,12 +1,13 @@
 package org.usth.ict.ulake.common.service;
 
-import javax.ws.rs.GET;
 import javax.ws.rs.HeaderParam;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
 import org.eclipse.microprofile.openapi.annotations.media.Schema;
+import org.eclipse.microprofile.openapi.annotations.parameters.RequestBody;
 import org.eclipse.microprofile.rest.client.inject.RegisterRestClient;
 import org.usth.ict.ulake.common.model.LakeHttpResponse;
 import org.usth.ict.ulake.common.model.acl.Acl;
@@ -15,15 +16,15 @@ import org.usth.ict.ulake.common.model.acl.Acl;
 @RegisterRestClient(configKey = "acl-api")
 @Produces(MediaType.APPLICATION_JSON)
 public interface AclService {
-    @GET
+    @POST
     @Path("/validation/file")
     @Schema(description = "validate file")
     public LakeHttpResponse validateFile(
-        @HeaderParam("Authorization") String bearer, Acl file);
+        @HeaderParam("Authorization") String bearer, @RequestBody Acl file);
 
-    @GET
+    @POST
     @Path("/validation/folder")
     @Schema(description = "validate folder")
     public LakeHttpResponse validateFolder(
-        @HeaderParam("Authorization") String bearer, Acl folder);
+        @HeaderParam("Authorization") String bearer, @RequestBody Acl folder);
 }
