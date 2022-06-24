37a193e5b79b033dec751807090cc2ed067b4c34
larycoder <lenhuchuhiep99@gmail.com>
2022-06-24 12:03:09 +0700

    core: [fix] return full error info in 500

--- core/src/main/java/org/usth/ict/ulake/core/resource/ObjectResource.java
+++ core/src/main/java/org/usth/ict/ulake/core/resource/ObjectResource.java
@@ -137,7 +137,7 @@ public class ObjectResource {
             cid = file.cid;
         } catch (Exception e) {
             log.error("File process error", e);
-            return response.build(500, "Internal error", e.toString());
+            return response.build(500, "Internal error", e);
         }
 
         return streamOutData(cid);
