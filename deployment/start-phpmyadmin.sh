33e650bb463f7a7a3c7db1358b3eadf178ab77d3
larycoder <hieplnc.m20ict@st.usth.edu.vn>
2022-06-24 11:26:26 +0700

    deployment: add phpmyadmin to ulake network

--- deployment/start-phpmyadmin.sh
+++ deployment/start-phpmyadmin.sh
@@ -4,12 +4,14 @@
 HOST="ulake-phpmyadmin";
 PORT="8081";
 PASSWORD="root"; # for root user
-SQL_HOST="172.17.0.1"; # default machine host in docker
-SQL_PORT="23306"; # follow start-mysql.sh
+SQL_HOST="ulake-mysql"; # default machine host in docker
+SQL_PORT="3306"; # follow start-mysql.sh
+NET="ulake-network";
 
 # Start phpmyadmin
 docker run --detach --name $HOST \
     --env MYSQL_ROOT_PASSWORD="$PASSWORD" \
     --env PMA_HOST=$SQL_HOST \
     --env PMA_PORT=$SQL_PORT \
+    --network $NET \
     -p $PORT:80 phpmyadmin:latest;
