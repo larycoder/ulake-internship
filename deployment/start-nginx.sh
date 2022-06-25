c0bcca180d2305d1fdfb6e8f1082ffc409ae9cbb
dx <doixanh@gmail.com>
2022-06-25 08:49:58 +0000

    deployment: Use a generalized start nginx script for all config or a specific frontend

--- deployment/start-nginx.sh
+++ deployment/start-nginx.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-# Start nginx docker with proper configuration and installation
-
 # Predefine variable
 HOST="ulake-nginx";
 PORT="8080";
@@ -10,14 +8,25 @@ ROOT_DIR=$(readlink -f $BASE_DIR/../);
 CONF="$BASE_DIR/nginx.conf";
 NET="ulake-network";
 
-DASHBOARD="$ROOT_DIR/dashboard/html";
-COMMON="$ROOT_DIR/common/html";
-ADMIN="$ROOT_DIR/admin/html";
-
-docker run --name $HOST -p $PORT:80 \
-    -v $CONF:/etc/nginx/nginx.conf:ro \
-    -v $DASHBOARD:/opt/dashboard:ro \
-    -v $COMMON:/opt/common:ro \
-    -v $ADMIN:/opt/admin:ro \
-    --network $NET \
-    -d nginx:latest
+if [ "$1" == "" ]; then
+    # Start nginx docker with ALL configurations and installations
+    DASHBOARD="$ROOT_DIR/dashboard/html";
+    COMMON="$ROOT_DIR/common/html";
+    ADMIN="$ROOT_DIR/admin/html";
+    TABLE="$ROOT_DIR/table/html";
+    docker run --name $HOST -p $PORT:80 \
+        -v $CONF:/etc/nginx/nginx.conf:ro \
+        -v $DASHBOARD:/opt/dashboard:ro \
+        -v $COMMON:/opt/common:ro \
+        -v $ADMIN:/opt/admin:ro \
+        -v $TABLE:/opt/table:ro \
+        --network $NET \
+        -d nginx:latest
+else
+    # Start nginx docker with a specific port and configurations
+    docker run --name $HOST-$1 -p $PORT:80 \
+        -v $CONF:/etc/nginx/nginx.conf:ro \
+        -v $ROOT_DIR/$1/html:/opt/dashboard:ro
+        --network $NET \
+        -d nginx:latest
+fi
