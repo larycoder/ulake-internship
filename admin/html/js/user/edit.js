dfe45ab2861947744a7714ce8688dc2d2a44bc98
dx <doixanh@gmail.com>
2022-07-01 16:01:37 +0000

    admin: Going to send back user info

--- admin/html/js/user/edit.js
+++ admin/html/js/user/edit.js
@@ -7,17 +7,30 @@ function detail(data) {
         info: false,
         aoColumns: [
             { mData: "key" },
-            { mData: "value", re }
+            { mData: "value", render: (data, type, row) =>
+                `<input class="form-control bg-light border-0 small" type="text" value="${data}" data-for="${row.key}" ${row.readonly ? "readonly" : ""}>` }
         ]
     });
 }
 
 async function userReady() {
-    const params = parseParam(uid, "/users");
+    const params = parseParam("uid", "/users");
     const uid = parseInt(params.uid);
     var userInfo = await userApi.one(uid);
     $("#username-detail").text(`Update User Detail for ${userInfo.userName}`);
-    detail(toTable(userInfo, "department, failedLogins, groups, firstName, lastName"));
+    const table = toTable(userInfo, "department, failedLogins, groups");
+    const readonly = "id,registerTime,userName,isAdmin".split(",");
+    table.forEach((data, index) => {
+        if (readonly.includes(data.key)) {
+            table[index].readonly = true;
+        }
+    });
+    detail(table);
+    $("#save").click(saveUser);
+}
+
+async function saveUser() {
+    showModal("Info", "Saving users");
 }
 
 $(document).ready(userReady);
