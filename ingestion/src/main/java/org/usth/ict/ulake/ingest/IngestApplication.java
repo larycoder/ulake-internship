21de2c5b5e528f53702bf2bbf0b0bf7592057328
larycoder <lenhuchuhiep99@gmail.com>
2022-07-02 16:49:48 +0700

    ingestion: [fix] foreget import package

--- ingestion/src/main/java/org/usth/ict/ulake/ingest/IngestApplication.java
+++ ingestion/src/main/java/org/usth/ict/ulake/ingest/IngestApplication.java
@@ -1,5 +1,8 @@
 package org.usth.ict.ulake.ingest;
 
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
 @ApplicationPath("/api")
 public class IngestApplication extends Application {
 }
