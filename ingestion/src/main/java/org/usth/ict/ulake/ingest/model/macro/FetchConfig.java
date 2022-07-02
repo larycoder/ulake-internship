1e9579521e78970a12d7f62f1ddffadf1f0e4da2
larycoder <lenhuchuhiep99@gmail.com>
2022-07-03 01:04:56 +0700

    ingestion: change FetchConfig to enum

--- ingestion/src/main/java/org/usth/ict/ulake/ingest/model/macro/FetchConfig.java
+++ ingestion/src/main/java/org/usth/ict/ulake/ingest/model/macro/FetchConfig.java
@@ -1,10 +1,3 @@
 package org.usth.ict.ulake.ingest.model.macro;
 
-public class FetchConfig {
-    public static final int POLICY = 0;
-    public static final int MODE = 1;
-
-    // fetcher mode
-    public static final int FETCH = 0;
-    public static final int DOWNLOAD = 1;
-}
+public enum FetchConfig { POLICY, MODE, FETCH, DOWNLOAD }
