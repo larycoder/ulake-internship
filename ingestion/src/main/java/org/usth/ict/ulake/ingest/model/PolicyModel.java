7bbf329d18b1072318e1c9e59ffb5c1ce1e6cbe7
larycoder <lenhuchuhiep99@gmail.com>
2022-07-03 00:03:52 +0700

    ingestion: policy model to crawl data

--- ingestion/src/main/java/org/usth/ict/ulake/ingest/model/PolicyModel.java
+++ ingestion/src/main/java/org/usth/ict/ulake/ingest/model/PolicyModel.java
@@ -5,25 +5,23 @@ import java.util.Map;
 
 public class PolicyModel {
     public abstract class Data {
-        public class Request {
-
-            public class Path {
-                public String value;
-                public Map<String, String> var;
-            }
-
-            public String method;
-            public Path path;
-        }
-
         public class Pattern {
             public String value;
+            public Map<String, String> var;
+        }
+
+        public class Request {
+            public String method;
+            public Pattern path;
+            public Pattern head;
+            public Pattern body;
         }
 
         public Request req;
         public Pattern pattern;
         public String map;
     }
+
     public Map<String, List<String>> declare;
     public Map<String, Data> pipe;
     public String pReturn;
