bf9bd5052fd997ad204a082651a46f98ca63673c
larycoder <lenhuchuhiep99@gmail.com>
2022-07-03 00:04:08 +0700

    ingestion: apply policy model to resource

--- ingestion/src/main/java/org/usth/ict/ulake/ingest/resources/CrawlResource.java
+++ ingestion/src/main/java/org/usth/ict/ulake/ingest/resources/CrawlResource.java
@@ -9,6 +9,9 @@ import javax.ws.rs.Path;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 import org.eclipse.microprofile.openapi.annotations.parameters.RequestBody;
 import org.usth.ict.ulake.ingest.model.DataModel;
 import org.usth.ict.ulake.ingest.model.PolicyModel;
@@ -19,12 +22,16 @@ public class CrawlResource {
     @Inject
     CrawlSvc svc;
 
+    @Inject
+    ObjectMapper mapper;
+
     @POST
     @Consumes(MediaType.APPLICATION_JSON)
     public DataModel runCrawl(
         @QueryParam("mode") String mode,
-        @RequestBody PolicyModel testPolicy) {
-        Map<String, Object> policy = null;
-        return svc.runCrawl(policy, mode);
+        @RequestBody PolicyModel policy) {
+        var type = new TypeReference<Map<String, Object>>(){};
+        var policyMap = mapper.convertValue(policy, type);
+        return svc.runCrawl(policyMap, mode);
     }
 }
