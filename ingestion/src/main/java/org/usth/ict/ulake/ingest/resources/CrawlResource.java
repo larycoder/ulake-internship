30ddecc48d9662e7ec73490ddc98494698b359ed
larycoder <lenhuchuhiep99@gmail.com>
2022-07-03 16:08:12 +0700

    ingestion: update Crawl system

--- ingestion/src/main/java/org/usth/ict/ulake/ingest/resources/CrawlResource.java
+++ ingestion/src/main/java/org/usth/ict/ulake/ingest/resources/CrawlResource.java
@@ -1,7 +1,5 @@
 package org.usth.ict.ulake.ingest.resources;
 
-import java.util.Map;
-
 import javax.inject.Inject;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.POST;
@@ -9,7 +7,7 @@ import javax.ws.rs.Path;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 
-import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import org.eclipse.microprofile.openapi.annotations.parameters.RequestBody;
@@ -30,8 +28,12 @@ public class CrawlResource {
     public DataModel runCrawl(
         @QueryParam("mode") String mode,
         @RequestBody Policy policy) {
-        var type = new TypeReference<Map<String, Object>>(){};
-        var policyMap = mapper.convertValue(policy, type);
+        String policyMap = null;
+        try {
+            policyMap = mapper.writeValueAsString(policy);
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+        }
         return svc.runCrawl(policyMap, mode);
     }
 }
