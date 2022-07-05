5ac7d9bac6de847c3643ea4d9a43fd94d4c98ef5
dx <doixanh@gmail.com>
2022-07-05 10:10:38 +0000

    admin: [WIP] add support for viewing compression task detail

--- admin/html/js/compress/view.js
+++ admin/html/js/compress/view.js
@@ -1,16 +1,44 @@
 // SSI CRUD: <!--# include virtual="/js/crud.js" -->
 
-const fileCrud = new CRUD({
-    api: fileApi,
-    listUrl: "/compress",
-    name: "Files",
-    nameField: "id",
-    listFieldRenderer: [
-        { mData: "id" },
-        { mData: "name" },
-        { mData: "size" },
-        { mData: "mime" }
-    ]
-});
 
-$(document).ready(() => fileCrud.listReady());
+/**
+ * Specific API for Compress Task management
+ */
+ class CompressTaskApi extends Api {
+    constructor (compressReqId) {
+        super(getCompressUrl(), "/api/compress")
+        this.compressReqId = compressReqId;
+    }
+
+    async all() {
+        return await this.get(`/${this.compressReqId}/files`);
+    }
+}
+
+function ready() {
+    const params = parseParam("id", "/compresses");
+    const compressReqid = parseInt(params.id);
+    const compressTaskApi = new CompressTaskApi(compressReqid);
+    const fileCrud = new CRUD({
+        api: compressTaskApi,
+        listUrl: "/compresses",
+        name: "Files",
+        nameField: "id",
+        listFieldRenderer: [
+            { mData: "id" },
+            { mData: "name" },
+            { mData: "size" },
+            { mData: "mime" }
+        ],
+        joins: {
+            apiMethod: (a) => fileApi.many(a),
+            fkField: "fileId",
+            targetId: "id",
+            targetField: "name"
+        }
+    });
+
+    fileCrud.listReady();
+}
+
+$(document).ready(ready);
