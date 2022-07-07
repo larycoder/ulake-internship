a15046164642e514c21eb910e3ceca1e7de67574
dx <doixanh@gmail.com>
2022-07-07 10:03:57 +0000

    deployment: Add some more logs...

--- deployment/start-service-all.sh
+++ deployment/start-service-all.sh
@@ -4,5 +4,6 @@ projects=$(cat ../settings.gradle  | grep include | sed "s/'//g;s/include //;s/,
 projects=($projects)
 echo ${projects[@]}
 for i in ${projects[@]}; do
+    echo Starting $i....
     ./start-service.sh -s $i
 done
