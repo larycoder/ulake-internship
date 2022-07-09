a894cfaa9dd11e4c3f9f6ba9cecc9fb07de4fcb6
dx <doixanh@gmail.com>
2022-07-09 03:38:04 +0000

    deployment: Allow starting service in a generic container using a mounted native runner

--- deployment/start-service.sh
+++ deployment/start-service.sh
@@ -8,6 +8,8 @@ NET="ulake-network"
 BASE_DIR=$(readlink -f $(dirname $0));
 ROOT_DIR=$(readlink -f $BASE_DIR/../);
 QUARKUS_SERVICE="";
+NATIVE=""
+NATIVE_CONTAINER="";
 
 # Parse args
 while test ${#} -gt 0; do
@@ -15,7 +17,11 @@ while test ${#} -gt 0; do
         -s) shift
             QUARKUS_SERVICE="$1"
             ;;
-        --help) echo "start-service.sh -s serviceName"
+        -n) shift
+            QUARKUS_SERVICE="$1"
+            ;;
+        --help) echo "start-service.sh -s serviceName [-n]"
+                echo "Option: -n for Native service"
                 exit
                 ;;
     esac
@@ -42,9 +48,19 @@ fi;
 #docker run --name ulake-lra -p 8793:8080 --network $NET -d jbosstm/lra-coordinator
 
 # Main services
-docker run --name $HOST \
-    -v $ROOT_DIR/common/src:/home/common/src \
-    -v $ROOT_DIR/$QUARKUS_SERVICE:/home/$QUARKUS_SERVICE \
-    -e QUARKUS_SERVICE=$QUARKUS_SERVICE \
-    --network $NET \
-    -d ulake/service:1.0.0-SNAPSHOT
+if [[ "$NATIVE" == "" ]]; then
+    docker run --name $HOST \
+        -v $ROOT_DIR/common/src:/home/common/src \
+        -v $ROOT_DIR/$QUARKUS_SERVICE:/home/$QUARKUS_SERVICE \
+        -e QUARKUS_SERVICE=$QUARKUS_SERVICE \
+        --network $NET \
+        -d ulake/service:1.0.0-SNAPSHOT
+else
+    RUNNER=`echo $ROOT_DIR/$QUARKUS_SERVICE/build/*-runner`
+    TARGET_RUNNER="/home/ulake-service-$QUARKUS_SERVICE-runner"
+    docker run -it --name $HOST \
+        -v $RUNNER:$TARGET_RUNNER \
+        --network $NET \
+        --entrypoint $TARGET_RUNNER \
+        registry.access.redhat.com/ubi8/ubi-minimal:8.6
+fi
