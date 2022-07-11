2425cacb9ca2142a2fc9235de1dc8a0b678fd246
dx <doixanh@gmail.com>
2022-07-11 11:17:46 +0000

    deployment: Update init.sql for creation of databases

--- deployment/init.sql
+++ deployment/init.sql
@@ -1,4 +1,19 @@
+# data lake core databases
 CREATE DATABASE IF NOT EXISTS `ulake-core`;
 CREATE DATABASE IF NOT EXISTS `ulake-folder`;
 CREATE DATABASE IF NOT EXISTS `ulake-user`;
 CREATE DATABASE IF NOT EXISTS `ulake-acl`;
+CREATE DATABASE IF NOT EXISTS `ulake-table`;
+
+
+# additional services databases
+CREATE DATABASE IF NOT EXISTS `ulake-search`;
+CREATE DATABASE IF NOT EXISTS `ulake-log`;
+CREATE DATABASE IF NOT EXISTS `ulake-ir`;
+CREATE DATABASE IF NOT EXISTS `ulake-extract`;
+CREATE DATABASE IF NOT EXISTS `ulake-compress`;
+CREATE DATABASE IF NOT EXISTS `ulake-ingestion`;
+
+# frontend services  databases
+CREATE DATABASE IF NOT EXISTS `ulake-admin`;
+
