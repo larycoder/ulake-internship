b97209f9ebb135a14bd892c56b4069fbeeafd713
dx <doixanh@gmail.com>
2022-07-11 15:12:40 +0000

    admin: Extends ListCRUD to DataCRFUD

--- admin/html/js/folders.js
+++ admin/html/js/folders.js
@@ -1,22 +1,22 @@
 import { ListCRUD } from "./crud/listcrud.js";
 import { userApi, folderApi } from "./api.js";
 
-window.crud = new ListCRUD({
-    api: folderApi,
-    listUrl: "/folders",
-    name: "Folder",
-    nameField: "name",
-    listFieldRenderer: [
-        { mData: "id" },
-        { mData: "name" },
-        { mData: "userName" }
-    ],
-    joins: {
-        apiMethod: (a) => userApi.many(a),
-        fkField: "ownerId",
-        targetId: "id",
-        targetField: "userName"
+class DataCRUD extends ListCRUD {
+    constructor() {
+        super({
+            api: folderApi,
+            listUrl: "/folders",
+            name: "Folders",
+            nameField: "name",
+            listFieldRenderer: [
+                { mData: "id" },
+                { mData: "name" },
+                { mData: "ownerId" }
+            ]});
+
     }
-});
+}
+
+window.crud = new DataCRUD();
 
 $(document).ready(() => window.crud.ready());
