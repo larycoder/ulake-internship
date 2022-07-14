967e68fec39f5171d0ee6d10bee0a8a6fb0f6d04
larycoder <lenhuchuhiep99@gmail.com>
2022-07-14 18:36:33 +0700

    ingestion: implement new generic fetcher instead of github fetcher

--- ingestion/src/main/java/org/usth/ict/ulake/ingest/crawler/fetcher/Fetcher.java
+++ ingestion/src/main/java/org/usth/ict/ulake/ingest/crawler/fetcher/Fetcher.java
@@ -1,14 +1,14 @@
 package org.usth.ict.ulake.ingest.crawler.fetcher;
 
-import java.util.List;
 import java.util.Map;
 
 import org.usth.ict.ulake.ingest.crawler.recorder.Recorder;
 import org.usth.ict.ulake.ingest.crawler.storage.Storage;
+import org.usth.ict.ulake.ingest.model.Policy;
 import org.usth.ict.ulake.ingest.model.macro.FetchConfig;
 
-public interface Fetcher<T, S> {
+public interface Fetcher<S, R> {
     void setup(Map<FetchConfig, String> config);
-    void setup(Storage<S> store, Recorder<T> consumer);
-    List<?> fetch();
+    void setup(Storage<S> store, Recorder<R> consumer);
+    Map<String, Object> fetch(Policy query);
 }
---
+++ ingestion/src/main/java/org/usth/ict/ulake/ingest/crawler/fetcher/impl/FetcherImpl.java
@@ -0,0 +1,41 @@
+package org.usth.ict.ulake.ingest.crawler.fetcher.impl;
+
+import java.io.InputStream;
+import java.util.Map;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.usth.ict.ulake.ingest.crawler.fetcher.Fetcher;
+import org.usth.ict.ulake.ingest.crawler.fetcher.cpl.Interpreter;
+import org.usth.ict.ulake.ingest.crawler.recorder.Recorder;
+import org.usth.ict.ulake.ingest.crawler.storage.Storage;
+import org.usth.ict.ulake.ingest.model.Policy;
+import org.usth.ict.ulake.ingest.model.macro.FetchConfig;
+
+public class FetcherImpl implements Fetcher<String, InputStream> {
+    private static final Logger log = LoggerFactory.getLogger(FetcherImpl.class);
+    private FetchConfig mode;
+
+    @Override
+    public void setup(Map<FetchConfig, String> config) {
+        mode = FetchConfig.valueOf(config.get(FetchConfig.MODE));
+    }
+
+    @Override
+    public void setup(Storage<String> store, Recorder<InputStream> consumer) {}
+
+    @Override
+    public Map<String, Object> fetch(Policy policy) {
+        Interpreter engine = new Interpreter();
+        var resultTable = engine.eval(policy);
+
+        if (mode == FetchConfig.FETCH) {
+            return resultTable.extractAsMap();
+        } else if (mode == FetchConfig.DOWNLOAD) {
+            // TODO: download and upload file to lake
+            return null;
+        } else {
+            return null;
+        }
+    }
+}
