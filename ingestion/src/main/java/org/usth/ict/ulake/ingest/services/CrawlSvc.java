3d081ca1978192bce4f72ec7b918a3b3a4f6246f
larycoder <lenhuchuhiep99@gmail.com>
2022-07-16 13:57:33 +0700

    ingestion: [fix] forget to configure recorder to fetcher and add record configuration relating to lake storage

--- ingestion/src/main/java/org/usth/ict/ulake/ingest/services/CrawlSvc.java
+++ ingestion/src/main/java/org/usth/ict/ulake/ingest/services/CrawlSvc.java
@@ -4,9 +4,12 @@ import java.util.HashMap;
 import java.util.Map;
 
 import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
 
+import org.eclipse.microprofile.rest.client.inject.RestClient;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.usth.ict.ulake.common.service.DashboardService;
 import org.usth.ict.ulake.ingest.crawler.fetcher.impl.FetcherImpl;
 import org.usth.ict.ulake.ingest.crawler.recorder.impl.ULakeCacheFileRecorderImpl;
 import org.usth.ict.ulake.ingest.model.Policy;
@@ -17,11 +20,18 @@ import org.usth.ict.ulake.ingest.model.macro.Record;
 public class CrawlSvc {
     private static final Logger log = LoggerFactory.getLogger(CrawlSvc.class);
 
-    public Map<String, Object> runCrawl(Policy policy, FetchConfig mode) {
+    @Inject
+    @RestClient
+    DashboardService svc;
+
+    public Map<String, Object> runCrawl(
+        Policy policy, FetchConfig mode, Long folderId, String token) {
         log.info("Setup recorder...");
-        var recorder = new ULakeCacheFileRecorderImpl();
+        var recorder = new ULakeCacheFileRecorderImpl(svc);
         Map<Record, String> recordConfig = new HashMap<>();
         recordConfig.put(Record.FILE_PATH, "/tmp/ulake");
+        recordConfig.put(Record.STORAGE_DIR, folderId.toString());
+        recordConfig.put(Record.TOKEN, token);
         recorder.setup(recordConfig);
 
         log.info("Setup fetcher...");
@@ -29,6 +39,7 @@ public class CrawlSvc {
         Map<FetchConfig, String> fetchConfig = new HashMap<>();
         fetchConfig.put(FetchConfig.MODE, mode.toString());
         fetcher.setup(fetchConfig);
+        fetcher.setup(null, recorder);
 
         log.info("Run fetcher...");
         return fetcher.fetch(policy);
