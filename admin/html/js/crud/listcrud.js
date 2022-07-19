c74ae51cc9e4b9967c7d47240afe05365f1aa5ed
dx <doixanh@gmail.com>
2022-07-19 06:43:11 +0000

    admin: Fix crashes when list crud does not have data in the table

--- admin/html/js/crud/listcrud.js
+++ admin/html/js/crud/listcrud.js
@@ -79,7 +79,7 @@ export class ListCRUD extends CRUD {
 
     async detail() {
         const data = await this.join(this.data);
-        if (!this.table && data && data.length) {
+        if (!this.table && data) {
             this.table = $('#table').DataTable(  {
                 data: data,
                 paging: true,
