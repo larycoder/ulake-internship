6553de620c3af32e36af4b4c7b7ae4b2bed6e288
larycoder <lenhuchuhiep99@gmail.com>
2022-07-19 16:19:56 +0700

    ingestion: update service to log process

--- ingestion/src/main/java/org/usth/ict/ulake/ingest/resources/CrawlResource.java
+++ ingestion/src/main/java/org/usth/ict/ulake/ingest/resources/CrawlResource.java
@@ -4,6 +4,7 @@ import java.util.Map;
 
 import javax.annotation.security.RolesAllowed;
 import javax.inject.Inject;
+import javax.transaction.Transactional;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.HeaderParam;
 import javax.ws.rs.POST;
@@ -13,35 +14,52 @@ import javax.ws.rs.core.MediaType;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+import org.eclipse.microprofile.jwt.JsonWebToken;
 import org.eclipse.microprofile.openapi.annotations.Operation;
 import org.eclipse.microprofile.openapi.annotations.parameters.Parameter;
 import org.eclipse.microprofile.openapi.annotations.parameters.RequestBody;
+import org.eclipse.microprofile.rest.client.inject.RestClient;
+import org.usth.ict.ulake.common.service.DashboardService;
 import org.usth.ict.ulake.ingest.model.Policy;
 import org.usth.ict.ulake.ingest.model.macro.FetchConfig;
+import org.usth.ict.ulake.ingest.persistence.FileLogRepo;
+import org.usth.ict.ulake.ingest.persistence.ProcessLogRepo;
 import org.usth.ict.ulake.ingest.services.CrawlSvc;
 
 @Path("/crawl")
 public class CrawlResource {
     @Inject
-    CrawlSvc svc;
+    ObjectMapper mapper;
 
     @Inject
-    ObjectMapper mapper;
+    @RestClient
+    DashboardService dashboardSvc;
+
+    @Inject
+    ProcessLogRepo processRepo;
+
+    @Inject
+    FileLogRepo fileRepo;
+
+    @Inject
+    JsonWebToken jwt;
 
     @POST
     @Consumes(MediaType.APPLICATION_JSON)
     @RolesAllowed({"User", "Admin"})
     @Operation(summary = "call crawl process")
+    @Transactional
     public Map<String, Object> crawl(
         @HeaderParam("Authorization") String token,
         @Parameter(description = "(FETCH to dry run, DOWNLOAD to crawl data")
-        @QueryParam("mode")
-        FetchConfig mode,
+        @QueryParam("mode") FetchConfig mode,
         @Parameter(description = "folder to store crawled files")
-        @QueryParam("folderId")
-        Long folderId,
-        @RequestBody(description = "instruction to execute crawl")
-        Policy policy) {
-        return svc.runCrawl(policy, mode, folderId, token);
+        @QueryParam("folderId") Long folderId,
+        @Parameter(description = "brief-description of crawl process")
+        @QueryParam("desc") String desc,
+        @RequestBody(description = "instruction of crawl") Policy policy) {
+
+        var svc = new CrawlSvc(dashboardSvc, jwt, processRepo, fileRepo);
+        return svc.runCrawl(policy, mode, folderId, desc);
     }
 }
--- ingestion/src/main/java/org/usth/ict/ulake/ingest/services/CrawlSvc.java
+++ ingestion/src/main/java/org/usth/ict/ulake/ingest/services/CrawlSvc.java
@@ -1,51 +1,121 @@
 package org.usth.ict.ulake.ingest.services;
 
+import java.io.InputStream;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
 import javax.enterprise.context.ApplicationScoped;
-import javax.inject.Inject;
 
-import org.eclipse.microprofile.rest.client.inject.RestClient;
+import org.eclipse.microprofile.jwt.JsonWebToken;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.usth.ict.ulake.common.service.DashboardService;
+import org.usth.ict.ulake.ingest.crawler.fetcher.Fetcher;
 import org.usth.ict.ulake.ingest.crawler.fetcher.impl.FetcherImpl;
+import org.usth.ict.ulake.ingest.crawler.recorder.Recorder;
 import org.usth.ict.ulake.ingest.crawler.recorder.impl.ULakeCacheFileRecorderImpl;
+import org.usth.ict.ulake.ingest.crawler.storage.Storage;
+import org.usth.ict.ulake.ingest.crawler.storage.impl.SqlLogStorageImpl;
+import org.usth.ict.ulake.ingest.model.IngestLog;
 import org.usth.ict.ulake.ingest.model.Policy;
+import org.usth.ict.ulake.ingest.model.ProcessLog;
 import org.usth.ict.ulake.ingest.model.macro.FetchConfig;
 import org.usth.ict.ulake.ingest.model.macro.Record;
+import org.usth.ict.ulake.ingest.persistence.FileLogRepo;
+import org.usth.ict.ulake.ingest.persistence.ProcessLogRepo;
 
-@ApplicationScoped
+/**
+ * Service to process crawl.
+ * Aware Behavior: not thread-safe
+ * */
 public class CrawlSvc {
     private static final Logger log = LoggerFactory.getLogger(CrawlSvc.class);
+    private DashboardService svc = null;
+    private JsonWebToken jwt;
+    private ProcessLogRepo processRepo;
+    private FileLogRepo fileRepo;
 
-    @Inject
-    @RestClient
-    DashboardService svc;
+    public class CrawlContext {
+        public Policy policy;
+        public FetchConfig mode;
+        public Long processId;
+        public Long folderId;
+        public Storage<IngestLog> storeObj;
+        public Recorder<InputStream> recordObj;
+        public Fetcher<IngestLog, InputStream> fetchObj;
+    }
+
+    private CrawlContext context = new CrawlContext();
+
+    public CrawlSvc(DashboardService svc, JsonWebToken jwt,
+                    ProcessLogRepo processRepo, FileLogRepo fileRepo) {
+        this.svc = svc;
+        this.jwt = jwt;
+        this.processRepo = processRepo;
+        this.fileRepo = fileRepo;
+    }
+
+    private void buildStore() {
+        context.storeObj = new SqlLogStorageImpl(processRepo, fileRepo);
+    }
+
+    private void buildRecord() {
+        context.recordObj = new ULakeCacheFileRecorderImpl(svc);
+        Map<Record, String> recordConfig = new HashMap<>();
+        recordConfig.put(Record.FILE_PATH, "/tmp/ulake");
+        recordConfig.put(Record.STORAGE_DIR, context.folderId.toString());
+        recordConfig.put(Record.TOKEN, jwt.getRawToken());
+        context.recordObj.setup(recordConfig);
+    }
+
+    private void buildFetch() {
+        context.fetchObj = new FetcherImpl();
+        Map<FetchConfig, String> fetchConfig = new HashMap<>();
+        fetchConfig.put(FetchConfig.MODE, context.mode.toString());
+        fetchConfig.put(FetchConfig.PROCESS_ID, context.processId.toString());
+        context.fetchObj.setup(fetchConfig);
+        context.fetchObj.setup(context.storeObj, context.recordObj);
+    }
 
     /**
      * Start crawl process.
      * Aware Behavior: file is stored to dir: "/tmp/ulake" before pushed to lake
      * */
     public Map<String, Object> runCrawl(
-        Policy policy, FetchConfig mode, Long folderId, String token) {
-        log.info("Setup recorder...");
-        var recorder = new ULakeCacheFileRecorderImpl(svc);
-        Map<Record, String> recordConfig = new HashMap<>();
-        recordConfig.put(Record.FILE_PATH, "/tmp/ulake");
-        recordConfig.put(Record.STORAGE_DIR, folderId.toString());
-        recordConfig.put(Record.TOKEN, token);
-        recorder.setup(recordConfig);
+        Policy policy, FetchConfig mode, Long folderId, String desc) {
 
-        log.info("Setup fetcher...");
-        var fetcher = new FetcherImpl();
-        Map<FetchConfig, String> fetchConfig = new HashMap<>();
-        fetchConfig.put(FetchConfig.MODE, mode.toString());
-        fetcher.setup(fetchConfig);
-        fetcher.setup(null, recorder);
+        context.policy = policy;
+        context.mode = mode;
+        context.folderId = folderId;
 
-        log.info("Run fetcher...");
-        return fetcher.fetch(policy);
+        log.info("Start crawl process...");
+        if (mode == FetchConfig.DOWNLOAD) {
+            var processLog = new ProcessLog();
+            processLog.ownerId = Long.parseLong(jwt.getName());
+            processLog.query = policy;
+            processLog.folderId = folderId;
+            processLog.description = desc;
+            processLog.creationTime = new Date().getTime();
+            processRepo.persist(processLog);
+            context.processId = processLog.id;
+        }
+
+        log.info("Setup crawl components...");
+        buildStore();
+        buildRecord();
+        buildFetch();
+
+        log.info("Run crawl process");
+        var resp = context.fetchObj.fetch(policy);
+
+        log.info("Done crawl process...");
+        if (mode == FetchConfig.DOWNLOAD) {
+            var processLog = processRepo.findById(context.processId);
+            processLog.endTime = new Date().getTime();
+            processRepo.persist(processLog);
+        }
+
+        return resp;
     }
 }
