69d07708e63688f6fc3e1b4149c6d1d8cc727136
dx <doixanh@gmail.com>
2022-07-24 04:42:50 +0000

    lcc: Allow fething data from backend to list

---
+++ lcc/rehtml/js/lcc.js
@@ -0,0 +1,163 @@
+$(document).ready(() => {
+    initNavbar();
+    routeReady();
+});
+
+function routeReady() {
+	const ready = {
+        "^/$": indexReady,
+        "^/dataset": datasetReady,
+        "^/patients": patientReady,
+        "^/team": teamReady,
+        "^/detect": detectReady,
+        "^/classify": classifyReady,
+		"^/cache": cacheReady
+	};
+	Object.keys(ready).forEach(r => {
+	    if (new RegExp(r).test(window.location.pathname)) ready[r]();
+	});
+}
+
+function initNavbar() {
+	$("span.user-name").text(getUserName() || "Xin chào!!");
+}
+
+// index functions
+function indexReady() {
+}
+
+// dataset functions
+function datasetReady() {
+}
+
+// patients functions
+function patientReady() {
+	window.setTimeout(function () {
+		$.ajax({
+			url: "/data/patients3cm",
+			success: data => {
+				const patients = $.csv.toObjects(data.replace(/^\s*[\r\n]/gm, ""));
+				if (Array.isArray(patients) && patients.length > 0) {
+					const t = document.querySelector("tbody");
+					while (t.firstChild) t.removeChild(t.firstChild);
+					patients.forEach(p => {
+						t.appendChild($(tplPatientRow(p))[0]);
+					});
+				}
+				$("h6 i.fa-spinner").remove();
+			}
+	})
+	}, 1000);
+}
+
+// detect functions
+function detectReady() {
+	const hrefs = window.location.href.split("/");
+	const detectPos = hrefs.indexOf("detect");
+	if (detectPos >= 0 && detectPos < hrefs.length - 1) {
+		const id = hrefs[detectPos + 1];
+		$.ajax({
+			url: `/service/detect/${id}`,
+			success: (data) => detectResultReady(data, id)
+		});
+	}
+}
+
+function detectResultReady(data, id) {
+	if (!data.pre_results || !Array.isArray(data.pre_results)) return;
+	const result = document.querySelector("#result");
+	while (result.firstChild) result.removeChild(result.firstChild);
+	let idx = 0;
+	data.pre_results.forEach(candidate => {
+		let c = {
+		    idx: ++idx,
+		    slice: candidate[0],
+            confidence: candidate[1].toFixed(2),
+            data: candidate[2],
+        };
+		result.appendChild($(tplDetectResultRow(c))[0]);
+	});
+
+	const summary = $("#summary");
+	summary.removeClass("invisible");
+    $.bindings({
+		count: data.pre_results.length,
+		name: id
+    });
+}
+
+// classify functions
+function classifyReady() {
+	const hrefs = window.location.href.split("/");
+	const detectPos = hrefs.indexOf("classify");
+	if (detectPos >= 0 && detectPos < hrefs.length - 1) {
+		const id = hrefs[detectPos + 1];
+		setTimeout(() => {
+			$.ajax({
+				url: `/service/classify/${id}`,
+				success: (data) => classifyResultReady(data, id)
+			})}, 6000);
+	}
+}
+
+function classifyResultReady(data, id) {
+	if (!data.pre_results || !Array.isArray(data.pre_results)) return;
+	const result = document.querySelector("#result");
+	while (result.firstChild) result.removeChild(result.firstChild);
+	let idx = 0;
+	data.pre_results.forEach(candidate => {
+		let c = {
+		    idx: ++idx,
+		    slice: candidate[0],
+            confidence: candidate[1].toFixed(2),
+            data: candidate[2],
+        };
+		result.appendChild($(tplClassifyResultRow(c))[0]);
+	});
+
+	const summary = $("#summary");
+	summary.removeClass("invisible");
+    $.bindings({
+		count: data.pre_results.length,
+		name: id
+    });
+}
+
+
+// contact functions
+function teamReady() {
+}
+
+
+let cacheId = [];
+function cacheReady() {
+	$.ajax({
+		url: "/data/patients3cm",
+		success: data => {
+			const patients = $.csv.toObjects(data.replace(/^\s*[\r\n]/gm, ""));
+			if (Array.isArray(patients) && patients.length > 0) {
+				patients.forEach(p => cacheId.push(p.ID) );
+				cacheId.slice(3);
+			}
+		}
+	});
+}
+
+function cacheLastId() {
+	if (cacheId.length > 0) {
+		let id = cacheId.pop();
+		console.log(`caching classify ${id}`);
+		$.ajax({
+			url: `/service/classify/${id}`,
+			success: data => {
+				console.log(`caching detect ${id}`);
+				$.ajax({
+					url: `/service/detect/${id}`,
+					success: data => {
+						cacheLastId();
+					}
+				})
+			}
+		})
+	}
+}
---
+++ lcc/rehtml/js/templates.js
@@ -0,0 +1,61 @@
+const tplDetectResultRow = (d) => `
+    <div class="row pb-3">
+        <div class="col-md-12">
+            <div class="row">
+                <div class="mx-auto"></div>
+                <div class="col-md-6">
+                    <hr/>
+                </div>
+                <div class="mx-auto"></div>
+            </div>
+
+            <div class="row pb-5">
+                <div class="col-md-12">
+                    Ứng viên số ${d.idx}
+                    <br/>
+                    Vị trí : lát cắt thứ ${d.slice}
+                    <br/>
+                    <img src="data:image/png;base64,${d.data}" class="full-width"/>
+                </div>
+            </div>
+        </div>
+    </div>
+`;
+
+const tplClassifyResultRow = (d) => `
+    <div class="row pb-3">
+        <div class="col-md-12">
+            <div class="row">
+                <div class="mx-auto"></div>
+                <div class="col-md-6">
+                    <hr/>
+                </div>
+                <div class="mx-auto"></div>
+            </div>
+
+            <div class="row pb-5">
+                <div class="col-md-12">
+                    Ứng viên số ${d.idx}
+                    <br/>
+                    Vị trí : lát cắt thứ ${d.slice}
+                    <br/>
+                    Xác suất ác tính : ${d.confidence} %
+                    <br/>
+                    <img src="data:image/png;base64,${d.data}" class="full-width"/>
+                </div>
+            </div>
+        </div>
+    </div>
+`;
+
+const tplPatientRow = (p) => `
+    <tr>
+        <td>${p.ID}</td>
+        <td>${p.ID}</td>
+        <td>${p.Sex}</td>
+        <td>${p.Modality}</td>
+        <td>${p.StudyDate}</td>
+        <td><a class="btn btn-sm btn-primary" href="/detect/${p.ID}">Định vị khối u</a></td>
+        <td><a class="btn btn-sm btn-primary" href="/classify/${p.ID}">Phân loại</a></td>
+    </tr>
+`;
