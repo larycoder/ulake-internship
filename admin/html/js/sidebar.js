2a76d98cd48c36d9c797fab0f2f1e55b952a444c
dx <doixanh@gmail.com>
2022-07-29 09:36:31 +0000

    admin: Disable admin section in sidebar by default for non-admin

--- admin/html/js/sidebar.js
+++ admin/html/js/sidebar.js
@@ -1,5 +1,8 @@
 function sidebarReady() {
     $(`a[class*='nav-link'][href='${window.location.pathname}']`).parent("li").addClass("active");
+    if (getGroups().includes("Admin")) {
+        $("#sidebar-admin").show();
+    }
 }
 
 $(document).ready(sidebarReady);
