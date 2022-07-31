4dfc48d61f35c95c545722d34b44908da71646d6
dx <doixanh@gmail.com>
2022-07-31 16:02:55 +0000

    ulake: Allow pressing enter on /login to start login

--- admin/html/js/login.js
+++ admin/html/js/login.js
@@ -4,6 +4,7 @@ window.login = async () => {
     const uid = document.querySelector("#uid").value,
           pwd = document.querySelector("#pwd").value;
 
+
     console.log(`Logging in as ${uid}, ${pwd}, domain ${getBaseDomain()}`);
     const button = $("form button");
     button.text("").append($(`<i class="fas fa-spinner fa-spin"></i>`));
@@ -25,3 +26,14 @@ window.login = async () => {
         button.text("Login")
     }
 }
+
+
+window.addEventListener('load', () => {
+    const pwd = document.querySelector("#pwd");
+    pwd.addEventListener('keypress', function (e) {
+        if (e.key === "Enter") {
+            e.preventDefault();
+            window.login();
+        }
+    });
+})
