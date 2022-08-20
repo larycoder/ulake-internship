24dfe9a0f3dd5c09f50442e43487f64d6ea4ec84
dx <doixanh@gmail.com>
2022-08-20 10:25:38 +0000

    admin: Add UI buttons to add row, delete row and save table

--- admin/html/js/table/detail.js
+++ admin/html/js/table/detail.js
@@ -4,6 +4,8 @@ function drawTable(resp) {
     const header = $("thead tr")
     $("#name-detail").html(`Content for table "${resp.name}"`);
 
+    resp.columns.unshift("id");
+    resp.columns.push("Actions");
     resp.columns.forEach(col => {
         const th = $("<th></th>");
         th.html(col);
@@ -12,10 +14,12 @@ function drawTable(resp) {
     // post process each row
     let rows = [];
     for (const rid in resp.rows) {
-        resp.rows[rid] = resp.rows[rid].map(cell => (`<input type="text" class="form-control form-control-sm" value="${cell}">`));
+        resp.rows[rid].unshift(rid);
+        resp.rows[rid] = resp.rows[rid].map((cell, idx) => idx === 0 ? cell : `<input type="text" class="form-control form-control-sm" value="${cell}">`);
+        resp.rows[rid].push(`<a href="#"><i class="fas fa-trash" onclick="window.deleteRow(this)"></i></a>
+                             <a href="#"><i class="fas fa-plus" onclick="window.addRow(this)"></i></a>`);
         rows.push(resp.rows[rid]);
     }
-    console.log(rows);
     $.fn.dataTable.ext.errMode = 'none';
     $('#table').DataTable({
         bProcessing: true,
@@ -30,6 +34,20 @@ async function ready() {
     const id = parseInt(params.id);
     const data = await tableApi.data(id);
     drawTable(data);
+}
+
+window.deleteRow = function(i) {
+    const table = $('#table').DataTable();
+    table.row($(i).parents('tr')).remove().draw();
+}
+
+window.addRow = function(i) {
+    const table = $('#table').DataTable();
+    table.row.add([]).draw();
+}
+
+window.save = function() {
+    console.log("Saving things right now!!");
 }
 
 $(document).ready(() => ready());
