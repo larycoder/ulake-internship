8e74272117c03c414bd83f37700dd338c2f7f495
dx <doixanh@gmail.com>
2022-08-21 07:15:54 +0000

    admin: OOP'ed table detail

--- admin/html/js/table/detail.js
+++ admin/html/js/table/detail.js
@@ -1,60 +1,67 @@
 import { tableApi } from "http://common.dev.ulake.usth.edu.vn/js/api.js";
 
-function drawTable(resp) {
-    const header = $("thead tr")
-    $("#name-detail").html(`Content for table "${resp.name}"`);
+class TableDetailCRUD {
+    drawTable(resp) {
+        const header = $("#table thead tr")
+        $("#name-detail").html(`Content for table "${resp.name}"`);
 
-    resp.columns.unshift("id");
-    resp.columns.push("Actions");
-    resp.columns.forEach(col => {
-        const th = $("<th></th>");
-        th.html(col.columnName);
-        header.append(th);
-    });
-    // post process each row
-    let rows = [];
-    for (const rid in resp.rows) {
-        resp.rows[rid].unshift(rid);
-        resp.rows[rid] = resp.rows[rid].map((cell, idx) => idx === 0 ? cell : `<input type="text" class="form-control form-control-sm" value="${cell}">`);
-        resp.rows[rid].push(`<a href="#"><i class="fas fa-trash" onclick="window.deleteRow(this)"></i></a>
-                             <a href="#"><i class="fas fa-plus" onclick="window.addRow(this)"></i></a>`);
-        rows.push(resp.rows[rid]);
+        resp.columns.unshift("id");
+        resp.columns.push("Actions");
+        resp.columns.forEach(col => {
+            const th = $("<th></th>");
+            th.html(col.columnName);
+            header.append(th);
+        });
+        // post process each row
+        let rows = [];
+        for (const rid in resp.rows) {
+            resp.rows[rid].unshift(rid);
+            resp.rows[rid] = resp.rows[rid].map((cell, idx) => idx === 0 ? cell : `<input type="text" class="form-control form-control-sm" value="${cell}">`);
+            resp.rows[rid].push(`<a href="#"><i class="fas fa-trash" onclick="window.crud.deleteRow(this)"></i></a>
+                                <a href="#"><i class="fas fa-plus" onclick="window.crud.addRow(this)"></i></a>`);
+            rows.push(resp.rows[rid]);
+        }
+        $.fn.dataTable.ext.errMode = 'none';
+        $('#table').DataTable({
+            bProcessing: true,
+            paging: true,
+            scrollX: true,
+            ordering: false,
+            data: rows
+        });
     }
-    $.fn.dataTable.ext.errMode = 'none';
-    $('#table').DataTable({
-        bProcessing: true,
-        paging: true,
-        scrollX: true,
-        ordering: false,
-        data: rows
-    });
+
+    async ready() {
+        const params = parseParam("id", "/tables");
+        const id = parseInt(params.id);
+        const data = await tableApi.data(id);
+        this.drawTable(data);
+    }
+
+    deleteRow(i) {
+        const table = $('#table').DataTable();
+        table.row($(i).parents('tr')).remove().draw();
+    }
+
+    addRow(i) {
+        const table = $('#table').DataTable();
+        table.row.add([]).draw();
+    }
+
+    save() {
+        console.log("Saving things right now!!");
+    }
+
+    saveColumn() {
+        console.log("Saving things right now!!");
+    }
+
+    stats() {
+        const params = parseParam("id", "/tables");
+        const id = parseInt(params.id);
+        window.location = `summary?id=${id}`
+    }
 }
 
-async function ready() {
-    const params = parseParam("id", "/tables");
-    const id = parseInt(params.id);
-    const data = await tableApi.data(id);
-    drawTable(data);
-}
-
-window.deleteRow = function(i) {
-    const table = $('#table').DataTable();
-    table.row($(i).parents('tr')).remove().draw();
-}
-
-window.addRow = function(i) {
-    const table = $('#table').DataTable();
-    table.row.add([]).draw();
-}
-
-window.save = function() {
-    console.log("Saving things right now!!");
-}
-
-window.stats = function () {
-    const params = parseParam("id", "/tables");
-    const id = parseInt(params.id);
-    window.location = `summary?id=${id}`
-}
-
-$(document).ready(() => ready());
+window.crud = new TableDetailCRUD();
+$(document).ready(() => window.crud.ready());
