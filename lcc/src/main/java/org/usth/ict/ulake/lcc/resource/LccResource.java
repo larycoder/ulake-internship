da8b5442fcd4fa8b4aaf25a76c455c31d081905c
larycoder <lenhuchuhiep99@gmail.com>
2022-08-22 15:26:47 +0700

    lcc: build quarkus service for loading data file

--- lcc/src/main/java/org/usth/ict/ulake/lcc/resource/LccResource.java
+++ lcc/src/main/java/org/usth/ict/ulake/lcc/resource/LccResource.java
@@ -1,5 +1,11 @@
 package org.usth.ict.ulake.lcc.resource;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 import javax.annotation.security.RolesAllowed;
 import javax.inject.Inject;
 import javax.transaction.Transactional;
@@ -14,11 +20,19 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
+import org.eclipse.microprofile.config.inject.ConfigProperty;
 import org.eclipse.microprofile.openapi.annotations.Operation;
+import org.eclipse.microprofile.rest.client.inject.RestClient;
 import org.usth.ict.ulake.common.misc.Utils;
 import org.usth.ict.ulake.common.model.LakeHttpResponse;
+import org.usth.ict.ulake.common.model.folder.FileModel;
+import org.usth.ict.ulake.common.service.CoreService;
+import org.usth.ict.ulake.common.service.DashboardService;
+import org.usth.ict.ulake.common.service.exception.LakeServiceForbiddenException;
+import org.usth.ict.ulake.common.service.exception.LakeServiceNotFoundException;
 import org.usth.ict.ulake.lcc.model.Patient;
 import org.usth.ict.ulake.lcc.persistence.PatientRepository;
+import org.usth.ict.ulake.lcc.service.FlaskAppService;
 
 @Path("/lcc")
 @Produces(MediaType.APPLICATION_JSON)
@@ -28,13 +42,68 @@ public class LccResource {
     PatientRepository repo;
 
     @Inject
+    @RestClient
+    CoreService core;
+
+    @Inject
+    @RestClient
+    DashboardService dashboard;
+
+    @Inject
+    @RestClient
+    FlaskAppService flaskApp;
+
+    @Inject
     LakeHttpResponse<Object> resp;
+
+    @ConfigProperty(name = "ulake.lcc.data")
+    String dataPath;
 
     @GET
     @RolesAllowed({ "Admin", "User" })
     @Operation(summary = "List all patients")
-    public Response all(@HeaderParam("Authorization") String bearer) {
+    public Response all() {
         return resp.build(200, "", repo.listAll());
+    }
+
+    @GET
+    @Path("/{patientId}/prediction")
+    @RolesAllowed({ "Admin", "User" })
+    @Operation(summary = "Predict patient image")
+    public Response predict(
+        @HeaderParam("Authorization") String bearer,
+        @PathParam("patientId") Long patientId) {
+        Patient patient = repo.findById(patientId);
+        if (patient == null)
+            return resp.build(404, "Patient not found");
+
+        FileModel fileInfo;
+        try {
+            fileInfo = dashboard.fileInfo(patient.fileId, bearer).getResp();
+        } catch (LakeServiceNotFoundException e) {
+            e.printStackTrace();
+            return resp.build(404, "Patient file not found");
+        } catch (LakeServiceForbiddenException e) {
+            e.printStackTrace();
+            return resp.build(403, "Patient file not found");
+        }
+
+        var filePath = Paths.get(dataPath, fileInfo.cid);
+        if (!Files.exists(filePath)) {
+            try {
+                File newFile = new File(filePath.toString());
+                newFile.createNewFile();
+                core.objectDataByFileId(patient.fileId, bearer).transferTo(
+                    new FileOutputStream(new File(filePath.toString())));
+            } catch (IOException e) {
+                e.printStackTrace();
+                return resp.build(500, "Fail to load file to local");
+            }
+        }
+
+        var flaskResp = flaskApp.predict(fileInfo.cid);
+        return resp.build(
+                   flaskResp.getCode(), flaskResp.getMsg(), flaskResp.getResp());
     }
 
     @POST
---
+++ lcc/src/main/java/org/usth/ict/ulake/lcc/service/FlaskAppService.java
@@ -0,0 +1,19 @@
+package org.usth.ict.ulake.lcc.service;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+
+import org.eclipse.microprofile.rest.client.annotation.RegisterProvider;
+import org.eclipse.microprofile.rest.client.inject.RegisterRestClient;
+import org.usth.ict.ulake.common.model.LakeHttpResponse;
+import org.usth.ict.ulake.common.service.LakeServiceExceptionMapper;
+
+@RegisterRestClient(configKey = "lcc-flaskapp-api")
+@RegisterProvider(value = LakeServiceExceptionMapper.class)
+public interface FlaskAppService {
+    @GET
+    @Path("/predict/{patientFile}")
+    public LakeHttpResponse<Object> predict(
+        @PathParam("patientFile") String patientFile);
+}
