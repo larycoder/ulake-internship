7ce1067ddcb0d932a9ce544bd9ebdce63d830edd
malenquillaa <dangviettung.works@gmail.com>
2024-04-17 15:49:32 +0700

    textr: simple quarkus CRUD

---
+++ textr/src/main/java/org/usth/ict/ulake/textr/model/User.java
@@ -0,0 +1,31 @@
+package org.usth.ict.ulake.textr.model;
+
+import io.quarkus.hibernate.orm.panache.PanacheEntityBase;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+public class User extends PanacheEntityBase {
+    @Column(length = 100)
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long Id;
+
+    @Column(length = 200)
+    private String Name;
+
+//    Setter
+    public void setName(String name) {
+        Name = name;
+    }
+
+//    Getter
+    public Long getId() {
+        return Id;
+    }
+
+    public String getName() {
+        return Name;
+    }
+}
