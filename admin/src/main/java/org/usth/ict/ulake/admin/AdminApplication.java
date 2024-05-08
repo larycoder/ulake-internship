e8d5e5f60319e61ef0c1aed83865d291830ccc98
NoVeTe36 <tungnv.bi12-480@st.usth.edu.vn>
2024-05-08 21:32:06 +0700

    admin: update quarkus and gradle version

--- admin/src/main/java/org/usth/ict/ulake/admin/AdminApplication.java
+++ admin/src/main/java/org/usth/ict/ulake/admin/AdminApplication.java
@@ -1,7 +1,7 @@
 package org.usth.ict.ulake.admin;
 
-import javax.ws.rs.ApplicationPath;
-import javax.ws.rs.core.Application;
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
 
 @ApplicationPath("/api")
 public class AdminApplication extends Application {
