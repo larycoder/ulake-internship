fd325461c3e564813fc6df1407324ed74e814fd3
NoVeTe36 <tungnv.bi12-480@st.usth.edu.vn>
2024-05-08 21:31:02 +0700

    acl:update quarkus and gradle version

--- acl/src/main/java/org/usth/ict/ulake/acl/persistence/AclRepo.java
+++ acl/src/main/java/org/usth/ict/ulake/acl/persistence/AclRepo.java
@@ -6,9 +6,9 @@ import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
-import javax.enterprise.context.ApplicationScoped;
-import javax.inject.Inject;
-import javax.persistence.EntityManager;
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
+import jakarta.persistence.EntityManager;
 
 import org.usth.ict.ulake.acl.model.AclModel;
 import org.usth.ict.ulake.common.misc.Utils;
