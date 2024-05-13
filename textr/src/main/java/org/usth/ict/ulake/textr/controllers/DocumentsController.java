a77b4a6863abac19cb9643416122a098e7f16ae9
malenquillaa <dangviettung.works@gmail.com>
2024-05-13 15:39:42 +0700

    textr: [update] re-design service structure

---
+++ textr/src/main/java/org/usth/ict/ulake/textr/controllers/DocumentsController.java
@@ -0,0 +1,81 @@
+package org.usth.ict.ulake.textr.controllers;
+
+import org.jboss.resteasy.annotations.providers.multipart.MultipartForm;
+import org.usth.ict.ulake.textr.models.Documents;
+import org.usth.ict.ulake.textr.models.EDocStatus;
+import org.usth.ict.ulake.textr.models.payloads.requests.MultipartBody;
+import org.usth.ict.ulake.textr.models.payloads.responses.MessageResponse;
+import org.usth.ict.ulake.textr.services.DocumentsService;
+
+import javax.inject.Inject;
+import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import java.util.List;
+
+@Path("/file")
+public class DocumentsController {
+
+    @Inject
+    DocumentsService documentsService;
+
+    @POST
+    @Path("/upload")
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.MULTIPART_FORM_DATA)
+    public Response upload(@MultipartForm MultipartBody body) {
+        MessageResponse messageResponse = documentsService.upload(body);
+
+        return Response.status(messageResponse.getStatus()).entity(messageResponse).build();
+    }
+
+    @GET
+    @Path("/list/stored")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response listAllStored() {
+        List<Documents> documents;
+        try {
+            documents = documentsService.listAllByStatus(EDocStatus.STATUS_STORED);
+        } catch (RuntimeException e) {
+            return Response.status(Response.Status.NOT_FOUND)
+                    .entity(new MessageResponse(404, e.getMessage()))
+                    .build();
+        }
+        return Response.ok(documents).build();
+    }
+
+    @GET
+    @Path("/list/deleted")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response listAllDeleted() {
+        List<Documents> documents;
+        try {
+            documents = documentsService.listAllByStatus(EDocStatus.STATUS_DELETED);
+        } catch (RuntimeException e) {
+            return Response.status(Response.Status.NOT_FOUND)
+                    .entity(new MessageResponse(404, e.getMessage()))
+                    .build();
+        }
+        return Response.ok(documents).build();
+    }
+
+    @POST
+    @Path("/delete/{id}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response delete(@PathParam("id") Long id) {
+        MessageResponse messageResponse = documentsService.updateStatusById(id, EDocStatus.STATUS_DELETED);
+
+        return Response.status(messageResponse.getStatus()).entity(messageResponse).build();
+    }
+
+    @POST
+    @Path("/restore/{id}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response restore(@PathParam("id") Long id) {
+        MessageResponse messageResponse = documentsService.updateStatusById(id, EDocStatus.STATUS_STORED);
+
+        return Response.status(messageResponse.getStatus()).entity(messageResponse).build();
+    }
+}
